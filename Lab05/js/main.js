803e8f8e1ae3a48f7ca9f3210466304c50948302
Wiktor Kurka <wiktor-kurka@protonmail.com>
2022-12-01 00:33:01 +0100

    Initialize lab05

---
+++ Lab05/js/main.js
@@ -0,0 +1,10 @@
+const asyncAdd = async (a,b) => {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+      return Promise.reject('Argumenty muszą mieć typ number!')
+    }
+    return new Promise((resolve, reject) => {
+      setTimeout(() =>{
+        resolve(a+b)
+      }, 100)
+    })
+  }
