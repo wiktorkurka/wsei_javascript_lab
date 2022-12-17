f387a663a5c63b9b358d4f1ef3cf1fa5ef801c56
Wiktor Kurka <wiktor-kurka@protonmail.com>
2022-12-17 17:41:32 +0100

    Submit lab5 solution

--- Lab05/js/main.js
+++ Lab05/js/main.js
@@ -1,10 +1,37 @@
-const asyncAdd = async (a,b) => {
-    if (typeof a !== 'number' || typeof b !== 'number') {
-      return Promise.reject('Argumenty muszą mieć typ number!')
-    }
-    return new Promise((resolve, reject) => {
-      setTimeout(() =>{
-        resolve(a+b)
-      }, 100)
-    })
+const asyncAdd = async (a, b) => {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    return Promise.reject('Argumenty muszą mieć typ number!')
   }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(a + b)
+    }, 100)
+  })
+}
+
+function addParams(...params) {
+  return params.reduce((promise, param) =>
+    promise.then(
+      (result) => asyncAdd(result, param)
+    ), Promise.resolve(0)
+  );
+}
+
+function benchmark(sampleSize, iterations = 10) {
+  let tBenchmarkMs = 0;
+
+  for (let i = 0; i < iterations; i++) {
+    let sample = [...Array(sampleSize)].map(() => (Math.random() * 100));
+
+    let tStart = performance.now();
+    addParams.apply(this, sample);
+
+    let tTotalMs = performance.now() - tStart;
+    console.log(`Execution took ${tTotalMs} ms`);
+
+    tBenchmarkMs += tTotalMs;
+  }
+
+  let tAverage = tBenchmarkMs / iterations;
+  console.log(`Benchmark done!\n\tSample size: ${sampleSize}\n\tSamlpes processed:${iterations}\n\tAverage time at ${tAverage} ms per sample`);
+}
